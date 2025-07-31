4969b56d68d3aa460ee0c539b4c0a38b77d1eb92
bachlx <luongxuanbach21112000@gmail.com>
2025-08-01 00:24:30 +0700

    navigation guard

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,9 +1,30 @@
 import Dashboard from '@/views/admin/Dashboard.vue';
 import CreatePost from '@/views/admin/CreatePost.vue';
 import EditPost from '@/views/admin/EditPost.vue';
+import AdminLayout from '@/layouts/AdminLayout.vue';
 
 export default [
-    { path: '/admin', component: Dashboard },
-    { path: '/admin/create', component: CreatePost },
-    { path: '/admin/edit/:id', component: EditPost, props: true },
+    {
+    path: '/admin',
+    component: AdminLayout,
+    meta: { requiresAuth: true, requiresAdmin: true },
+    children: [
+      {
+        path: '',
+        name: 'AdminDashboard',
+        component: Dashboard
+      },
+      {
+        path: 'create',
+        name: 'CreatePost',
+        component: CreatePost
+      },
+      {
+        path: 'edit/:id',
+        name: 'EditPost',
+        component: EditPost,
+        props: true
+      }
+    ]
+  }
 ]
